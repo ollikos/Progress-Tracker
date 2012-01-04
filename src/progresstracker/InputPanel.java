c615feb2ab59eac489ddd3da82f04b14d0e611f6
Olli Koskinen <mudface@gmail.com>
2012-01-04 08:03:09 +0200

    Added license and fixed project templates.

--- src/progresstracker/InputPanel.java
+++ src/progresstracker/InputPanel.java
@@ -1,16 +1,31 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2012 Olli Koskinen <olli.koskinen@metropolia.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
+/**
+ *
+ * @author Olli Koskinen <olli.koskinen@metropolia.fi>
+ */
+
 package progresstracker;
 
 import java.awt.Graphics;
 import javax.swing.*;
 
-/**
- *
- * @author Olli Koskinen
- */
 public class InputPanel extends JPanel{
     private JLabel numSets;  //The number of sets
     private JTextField numSetsField;
